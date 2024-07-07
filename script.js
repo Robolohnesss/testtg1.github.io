44aa1cc542674d5e625fca021b0b3a10e3fe88db
Robolohnesss <93622213+Robolohnesss@users.noreply.github.com>
2024-07-07 19:18:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let tg = window.Telegram.WebApp;
-
+try{
 document.getElementById("my_text").textContent = tg.initDataUnsafe.user.first_name;
+}
+catch{
+    alert("Open in tg")
+}
+
+if(tg.initDataUnsafe.user.id == 'robolohness'){
+    document.getElementById("my_text").textContent = 'вы админ';
+}
